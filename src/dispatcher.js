0ec4ab8ad57c668d1b883099d618d67d0a666651
Per Guth <mail@perguth.de>
2015-11-06 18:22:04 +0100

    mv UI ops to separate file

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,10 +1,11 @@
-var inherits = require( 'inherits')
+var algorithm = 'aes-256-ctr'
+var crypto = require( 'crypto')
+var detect = require( 'feature/detect')
 var EventEmitter = require( 'events').EventEmitter
 var FileReadStream = require( 'namedfilestream/read')
 var FileWriteStream = require( './mod-namedfilestream/write')
-var detect = require( 'feature/detect')
-var crypto = require( 'crypto')
-var algorithm = 'aes-256-ctr'
+var inherits = require( 'inherits')
+var ui = require( './ui')()
 
 module.exports = Dispatcher
 
@@ -13,14 +14,13 @@ function Dispatcher (opts){
   if (! (this instanceof Dispatcher)) return new Dispatcher( opts)
   
   var urlElem = document.getElementById( 'url')
-  var uploadbtn = document.getElementById( 'uploadLink')
   var mesh
 
   this.on( 'noHash', x=>{
     this.emit( 'initMesh') })
 
   this.on( 'hash', hash =>{
-    let parts = hash.split(':', 2)
+    let parts = hash.split( ':', 2)
     this.emit( 'initMesh', ...parts)
     urlElem.value = window.location.href.toString() })
 
@@ -48,31 +48,8 @@ function Dispatcher (opts){
 
   this.on( 'updateSendButton', x=>{
     let peers = mesh.wrtc.peers.length
-    if (peers <= 0){
-      uploadbtn.style[ 'cursor'] = 'default'
-      uploadbtn.style[ 'opacity'] = '.6'
-      uploadbtn.onclick = x=> null
-      uploadbtn.className =
-        uploadbtn.className.replace( 'process', 'receive' )
-      uploadbtn.className =
-        uploadbtn.className.replace( 'green', 'magenta')
-      uploadbtn.className =
-        uploadbtn.className.replace( 'red', 'magenta')
-      uploadbtn.text = 'reinitializing' }
-    else {
-      uploadbtn.style[ 'cursor'] = 'pointer'
-      uploadbtn.style[ 'opacity'] = '1'
-      uploadbtn.onclick = x=> document.getElementById('send').click()
-      uploadbtn.className =
-        uploadbtn.className.replace( 'receive', 'send')
-      uploadbtn.className =
-        uploadbtn.className.replace( 'process', 'send')
-      uploadbtn.className =
-        uploadbtn.className.replace( 'magenta', 'green')
-      uploadbtn.className =
-        uploadbtn.className.replace( 'red', 'green')
-      uploadbtn.text = 'send to ' + peers + ' peer'
-        + ((peers > 1)?'s':'') } })
+    if (peers <= 0) ui.emit( 'noMorePeers', peers)
+    else ui.emit( 'peersAvailable', peers) })
 
   this.on( 'connect', peer =>{
     console.log( 'peer connected:', peer)
@@ -89,49 +66,29 @@ function Dispatcher (opts){
       this.emit( 'attachFileURL', file) }) })
 
   this.on( 'handleDownload', (meta, peer, writeStream, decrypt) =>{
+    console.log( 'incoming file size:', meta.size)
     let shasum = crypto.createHash( 'sha1').update( meta.name)
     let fileID = shasum.digest( 'hex')
-    this.emit( 'addFileButton', meta.name, fileID)
-    console.log( 'incoming file size:', meta.size)
+    ui.emit( 'addFileButton', meta.name, fileID)
     writeStream.on( 'progress', progress =>{
       this.emit( 'endWriteStream',
         writeStream, peer, decrypt, meta.size, progress, fileID)
-      this.emit( 'updateProgress', meta.name, fileID, meta.size, progress) }) })
+      ui.emit( 'updateProgress', meta.name, fileID, meta.size, progress) }) })
 
   this.on( 'endWriteStream',
     (writeStream, peer, decrypt, overall, progress, fileID) =>{
     if (overall <= progress) {
       writeStream.end()
       decrypt.end()
-      let fileButton = document.getElementsByClassName( fileID)[ 0]
-      fileButton.className =
-        fileButton.className.replace( 'receive', 'browse')
+      ui.emit( 'fileReceived', fileID)
       this.emit( 'acceptFiles', peer) } })
-
-  this.on( 'addFileButton', (fileName, fileID) =>{
-    console.log( 'fileid', fileID)
-    let filesArea = document.getElementById('files')
-    filesArea.innerHTML =
-      `<a id=downloadLink class='button receive red ${fileID}' style='cursor:default;
-        width:100%;height:62px;line-height:62px;margin-bottom:13px;
-        text-transform:none;opacity:1;background-image:url(green-big.png);
-        background-repeat:no-repeat;background-position-x:-436px;'
-        target=_blank>${fileName}</a>${filesArea.innerHTML}` })
-
-  this.on( 'updateProgress', (name, fileID, overall, size) =>{
-    let fileButton = document.getElementsByClassName( fileID)[ 0]
-    let margin = -1 * (436 - 436 * size / overall)
-    fileButton.style.backgroundPositionX = margin + 'px' })
 
   this.on( 'attachFileURL', file =>{
     let shasum = crypto.createHash( 'sha1')
     shasum.update( file.name)
     let fileID = shasum.digest( 'hex')
-    let fileButton = document.getElementsByClassName( fileID)[ 0]
     let fileLink = detect( 'URL').createObjectURL( file)
-    fileButton.href = fileLink
-    fileButton.download = file.name
-    fileButton.style.cursor = 'pointer' })
+    ui.emit( 'attachFileURL', fileID, fileLink, file.name) })
 
   this.on( 'fileAdded', input =>{
     let file = new FileReadStream( input, {fields: ['name', 'size', 'type']})
@@ -139,16 +96,7 @@ function Dispatcher (opts){
 
   this.on( 'sendFile', (file, peer) =>{
     console.log( 'sending file to peer:', peer)
-    uploadbtn.style[ 'cursor'] = 'default'
-    uploadbtn.onclick = x=> null
-    uploadbtn.text = 'sending'
-    uploadbtn.className =
-      uploadbtn.className.replace( 'green', 'red')
-    uploadbtn.className =
-      uploadbtn.className.replace( 'send', 'process' )
-    let encrypt = crypto.createCipher(algorithm, mesh.password)
+    ui.emit( 'sendFile')
+    let encrypt = crypto.createCipher( algorithm, mesh.password)
     file.pipe( encrypt).pipe( peer, {end: false})
-    file.on( 'end', x=>{
-      this.emit( 'updateSendButton')
-      uploadbtn.onclick = x=> document.getElementById('send').click()
-      console.log( 'sadly this would also ends the peer stream :-(') }) }) }
+    file.on( 'end', x=> this.emit( 'updateSendButton')) }) }
---
+++ src/ui.js
@@ -0,0 +1,73 @@
+var inherits = require( 'inherits')
+var EventEmitter = require( 'events').EventEmitter
+
+module.exports = Ui
+
+inherits( Ui, EventEmitter)
+function Ui (opts){
+  if (! (this instanceof Ui)) return new Ui( opts)
+  
+  var urlElem = document.getElementById( 'url')
+  var uploadbtn = document.getElementById( 'uploadLink')
+
+  this.on( 'noMorePeers', peers =>{
+    uploadbtn.style[ 'cursor'] = 'default'
+      uploadbtn.style[ 'opacity'] = '.6'
+      uploadbtn.onclick = x=> null
+      uploadbtn.className =
+        uploadbtn.className.replace( 'process', 'receive' )
+      uploadbtn.className =
+        uploadbtn.className.replace( 'green', 'magenta')
+      uploadbtn.className =
+        uploadbtn.className.replace( 'red', 'magenta')
+      uploadbtn.text = 'reinitializing' })
+
+  this.on( 'peersAvailable', peers =>{
+      uploadbtn.style[ 'cursor'] = 'pointer'
+      uploadbtn.style[ 'opacity'] = '1'
+      uploadbtn.onclick = x=> document.getElementById('send').click()
+      uploadbtn.className =
+        uploadbtn.className.replace( 'receive', 'send')
+      uploadbtn.className =
+        uploadbtn.className.replace( 'process', 'send')
+      uploadbtn.className =
+        uploadbtn.className.replace( 'magenta', 'green')
+      uploadbtn.className =
+        uploadbtn.className.replace( 'red', 'green')
+      uploadbtn.text = 'send to ' + peers + ' peer'
+        + ((peers > 1)?'s':'') })
+
+  this.on( 'fileReceived', fileID =>{
+    let fileButton = document.getElementsByClassName( fileID)[ 0]
+    fileButton.className =
+      fileButton.className.replace( 'receive', 'browse') })
+
+  this.on( 'addFileButton', (fileName, fileID) =>{
+    console.log( 'fileid', fileID)
+    let filesArea = document.getElementById('files')
+    filesArea.innerHTML =
+      `<a id=downloadLink class='button receive red ${fileID}' style='cursor:default;
+        width:100%;height:62px;line-height:62px;margin-bottom:13px;
+        text-transform:none;opacity:1;background-image:url(green-big.png);
+        background-repeat:no-repeat;background-position-x:-436px;'
+        target=_blank>${fileName}</a>${filesArea.innerHTML}` })
+
+  this.on( 'updateProgress', (name, fileID, overall, size) =>{
+    let fileButton = document.getElementsByClassName( fileID)[ 0]
+    let margin = -1 * (436 - 436 * size / overall)
+    fileButton.style.backgroundPositionX = margin + 'px' })
+
+  this.on( 'attachFileURL', (fileID, fileLink, fileName) =>{
+    let fileButton = document.getElementsByClassName( fileID)[ 0]
+    fileButton.href = fileLink
+    fileButton.download = fileName
+    fileButton.style.cursor = 'pointer' })
+
+  this.on( 'sendFile', x=>{
+    uploadbtn.style[ 'cursor'] = 'default'
+    uploadbtn.onclick = x=> null
+    uploadbtn.text = 'sending'
+    uploadbtn.className =
+      uploadbtn.className.replace( 'green', 'red')
+    uploadbtn.className =
+      uploadbtn.className.replace( 'send', 'process') }) }
